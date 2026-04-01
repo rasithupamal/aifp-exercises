7b75853f5516b3e4a9d96cf6f7ac00fb280f1f07
Rasith Upamal <rasith@i4tlabs.io>
2026-04-01 10:43:49 +0530

    ex02_age_next_year.py

---
+++ ex02_age_next_year.py
@@ -0,0 +1,8 @@
+# Ask the user for their current age
+age = input("Enter your age : ");
+
+# convert the input to an integer
+age = int(age);
+
+# calculate and print next year's age
+print(f"Next year you will be {age + 1}.");
